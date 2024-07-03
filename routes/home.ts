51aca72106ee84d16bcc35be55608175e2fc2c59
Shivam <shivamguptanitw@gmail.com>
2024-07-04 01:59:03 +0545

    Railway Deploy Check

--- routes/home.ts
+++ routes/home.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
+import { homeController } from "../controller/home/home.controller";
 
 const router = Router();
+
+// Home page route
+router.get("/", homeController)
 
 
 export default router;
